561acd913b3ab55034cf3962429dde12b5bb60d6
Paulo Henrique Lima <limapaulobsb@gmail.com>
2022-04-20 11:01:47 -0300

    Add cors and fix eslint config

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 
 const express = require('express');
+const cors = require('cors');
 const loginRouter = require('./controllers/loginController');
 const userRouter = require('./controllers/userController');
 const errorMiddleware = require('./middlewares/errorMiddleware');
@@ -9,11 +10,18 @@ const port = process.env.PORT;
 
 const app = express();
 
+app.use(cors());
+
 app.use(express.json());
 
+// app.get('/', (_req, res) => res.status(200).send('User Profile'));
+
 app.use('/login', loginRouter);
+
 app.use('/users', userRouter);
 
 app.use(errorMiddleware);
 
-app.listen(port, () => console.log(`Listening on port ${port}`));
+app.listen(port, () => {
+  console.log(`CORS-enabled web server listening on port ${port}`);
+});
